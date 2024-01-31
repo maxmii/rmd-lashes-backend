6309e37e4e38012009f51eeb8e30e7dc85cca99b
maxamills <morrm046@gmail.com>
2024-01-31 19:52:24 +0000

    Add Django Admin customisation and Testcases  - ordering with ID  - List only name and email for now
      Add Tests for admin.py to test user list with admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""
+Test for the Django admin modifications
+"""
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Tests for django admin."""
+
+    def setUp(self):
+        """Create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='Test123',
+        )
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com', password='Test123', name='Test User'
+        )
+
+    def test_user_list(self):
+        """Test that users are listed on page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
