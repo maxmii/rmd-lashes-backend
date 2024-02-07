e767850ab7fd06a30f9c3bf358b03a9ff13ca4f9
Max Morris <35525228+maxmii@users.noreply.github.com>
2024-02-07 00:16:36 +0000

    Merge changes from dev branch into main
    * Use python:3.11.7-alpine3.19 for dockerfile
    
    * Fix linting issue in admin.py and urls.py
    
    Add noqa comment to ignore linting error for unused import
    in admin.py file  and urls.py to pass linting checks.
    
    * Fix flak8 lint
    
    * Remove views.py
    
    * Improve test coverage for database management command
    
    Create test cases for wait for db command in CommandTests class
    
    Add testcases
     - test_wait_for_db_ready
     - test_for_db_delay
    
    * Refactor user model definition
    
    Added 'is_staff' field and corresponding code for staff user management,
    aligning with the project requirements. This change enhances user
    control and access management.
    
    Add is_staff as tests were failing due it being missing
    
    * Add Django Admin customisation and Testcases
     - ordering with ID
     - List only name and email for now
    
      Add Tests for admin.py to test user list with admin
    
    * Refactor Django admin for User model customization
    
    Add Create User page
    Add Edit User Page
    
    Create tests for this
     - test_edit_user_page: To test edit user page
     - test_create_user_page: To test create user page
    
    * Create Unit tests for User API
    
    * Create User API
      - Created URLs for the User API
      - Created the Serializers for User API
      - Created the View for User API
      - Create Tokens for users
      - Use drf-spectacular for API views
      - Create a SwaggerAPI View for APIs
    
    * Remove trailing slash for user urls as not needed
    Fix api schema path
    
    * Create two django Apps Bookings and Services
    
      - Create Migrations for Booking and Services
      - Create Django template for Bookings
      - Create Django template for services
      - Add apps Booking and Services to django settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'user',
+    'bookings',
+    'services',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +136,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'}
--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,9 @@
 Models for database
 """
 
+import datetime
+from django.core.validators import MinValueValidator
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -37,8 +40,46 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Bookings(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    booking_starttime = models.DateTimeField(
+        validators=[MinValueValidator(datetime.datetime.now())]
+    )
+    booking_endtime = models.DateTimeField(
+        validators=[MinValueValidator(datetime.datetime.now())]
+    )
+    booking_duration = models.DurationField()
+    booking_duration = models.DurationField()
+    booking_notes = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    booking_cancelled = models.BooleanField(default=False)
+    booking_completed = models.BooleanField(default=False)
+    service_id = models.ForeignKey('Services', on_delete=models.CASCADE)
+
+
+class Services(models.Model):
+    SERVICE_CHOICES = {
+        "LASHES": "Lashes",
+        "NAILS": "Nails",
+        "BROWS": "Brows",
+    }
+    service_id = models.AutoField(primary_key=True)
+    service_name = models.CharField(max_length=255)
+    service_desciption = models.TextField(blank=True)
+    service_duration = models.DurationField()
+    service_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    service_type = models.CharField(choices=SERVICE_CHOICES)
+
+    def __str__(self):
+        """Return service name"""
+        return self.service_name
